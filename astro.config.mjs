f2304d2f62ab470e05787bc48f1936bdda4b0e05
Tom Plüss <tom.pluess@simplificator.com>
2023-04-22 14:15:51 +0200

    Integrate tailwind
    generated with `npx astro add tailwind`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   image: {
-    service: "astro/assets/services/sharp",
+    service: "astro/assets/services/sharp"
   },
+  integrations: [tailwind()]
 });
