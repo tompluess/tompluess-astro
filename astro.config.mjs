5b920fe1b84c624018a731aceb0f18c86a7e9b9f
Tom Plüss <tom.pluess@simplificator.com>
2023-05-19 16:44:08 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
+    sitemap(),
   ],
 });
