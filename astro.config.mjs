9604d2b7a3a74c6cf707f06b578716c2603a2cfb
Tom Plüss <tom@tompluess.ch>
2024-01-01 13:32:21 +0100

    Upgrade to astro 4
    Migrate to built-in image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-starter-tailwindcss.vercel.app/",
   integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     tailwind({
       config: {
         applyBaseStyles: false,
